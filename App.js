4da6f291fbf1ee8f2388066830615b822a6cfba7
Linda Zhou <lz28330@gmail.com>
2018-11-15 15:48:51 -0500

    change colors, add img, organize code cleaner

--- App.js
+++ App.js
@@ -47,12 +47,26 @@ export default class App extends Component {
     const imageInfo = {
       uri: 'https://calculate-this.com/sites/default/files/styles/large/public/field/image/pizza.png?itok=yGvUTlve'
     }
+    const donutImage = {
+      uri: 'http://pluspng.com/img-png/doughnut-png-hd-various-flavours-600.png'
+    }
     // <Text style={styles.instructions}>To get started, edit App.js</Text>
     // <Text style={styles.instructions}>{instructions}</Text>
     return (
       <View style={styles.container}>
-        <Text style={[styles.welcome, this.state.customStyles]}>Hello! Welcome to React Native!</Text>
-        <Image source={imageInfo} style={{width: 400, height: 400}}/>
+        <View style={styles.half1}>
+          <Text style={[styles.welcome, this.state.customStyles]}>Hello! Welcome to React Native!</Text>
+        </View>
+        <View style={styles.half2}>
+          <View style={[styles.half21, styles.half2x]}>
+            <Text style={{color: 'white'}}>Pizza</Text>
+            <Image source={imageInfo} style={{width: 100, height: 100}}/>
+          </View>
+          <View style={[styles.half22, styles.half2x]}>
+            <Text>Donut</Text>
+            <Image source={donutImage} style={{width: 100, height: 100}}/>
+          </View>
+        </View>
       </View>
     );
   }
@@ -62,15 +76,37 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //for columns View:
+    //flexDirection: 'row',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // backgroundColor: '#F5FCFF',
+  },
+  half1: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  half2: {
+    flex: 2,
+    flexDirection: 'row',
+    backgroundColor: 'green',
+  },
+  half2x: {
+    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
+  },
+  half21: {
+    backgroundColor: 'brown',
+  },
+  half22: {
+    backgroundColor: 'pink',
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
-    color: 'blue',
+    marginTop: 50,
+    color: 'orange',
   },
   instructions: {
     textAlign: 'center',
